b03233dd1cc4937614054dd3bf6ab44ed5b528ca
hamzah <shaikhhamzah93@gmail.com>
2025-04-10 11:42:35 +0530

    Added migration to create foreign key relationship between City and Airport models

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { serverConfig, Logger } = require('./config')
 const express = require('express')
 const apiRoutes = require('./routes')
+const { City, Airport } = require('./models')
+const { where } = require('sequelize')
 
 const app = express()
 
@@ -9,7 +11,18 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRoutes)
 
+async function db() {
+    // const city = await City.findByPk(7)
+    // await city.createAirport({ 'name': 'Kempagowda Airport', code: 'KMP' })
+    await City.destroy({
+        where: {
+            id: 7
+        }
+    })
+}
+
 app.listen(serverConfig.PORT, () => {
     console.log(`Server running at port ${serverConfig.PORT}`)
     Logger.info('Successfully started the server')
+    db()
 })
